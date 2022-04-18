646804cfc5a4321db902c110b330feefdd42adbb
ragi <ragi@github.com>
2022-04-18 14:26:03 +0200

    initial structure

---
+++ src/Roebi/PatientManagment/Api/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Roebi.RoomManagment.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomController : ControllerBase
+    {
+        private readonly ILogger<RoomController> _logger;
+
+        public RoomController(ILogger<RoomController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetRooms")]
+        public string Get()
+        {
+            return "asdasd";
+        }
+    }
+}
