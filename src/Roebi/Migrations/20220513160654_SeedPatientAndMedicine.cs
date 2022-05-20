2b8bb2cd1e25e756565d6bd423fcd3b979bc2a15
ragi <ragi@github.com>
2022-05-20 16:06:12 +0200

    seed data patient and medicament

---
+++ src/Roebi/Migrations/20220513160654_SeedPatientAndMedicine.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roebi.Migrations
+{
+    public partial class SeedPatientAndMedicine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Medicine(Name) Values ('Medikament 1')");
+            migrationBuilder.Sql("INSERT INTO Medicine(Name) Values ('Medikament 2')");
+            migrationBuilder.Sql("INSERT INTO Medicine(Name) Values ('Medikament 3')");
+            migrationBuilder.Sql("INSERT INTO Medicine(Name) Values ('Medikament 4')");
+
+            migrationBuilder.Sql("INSERT INTO Patient(LastName,Firstname,EntryStamp,ExitStamp,CaseHistory,RoomId) Values ('Müller','Peter', 1653055234, 0, 'Hat Kopfschmerzen', 1)");
+            migrationBuilder.Sql("INSERT INTO Patient(LastName,Firstname,EntryStamp,ExitStamp,CaseHistory,RoomId) Values ('Solomon','Marius', 1653055234, 0, 'Hat Bauchschmerzen', 2)");
+            migrationBuilder.Sql("INSERT INTO Patient(LastName,Firstname,EntryStamp,ExitStamp,CaseHistory,RoomId) Values ('Rogers','Damian', 1653055234, 0, 'Hat Gliederschmerzen', 3)");
+            migrationBuilder.Sql("INSERT INTO Patient(LastName,Firstname,EntryStamp,ExitStamp,CaseHistory,RoomId) Values ('Ritter','Kane', 1653055234, 0, 'Hat ein gebrochenes Bein', 4)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("TRUNCATE Medicine");
+            migrationBuilder.Sql("TRUNCATE Patient");
+        }
+    }
+}
